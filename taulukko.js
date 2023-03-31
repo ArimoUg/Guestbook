04e171c15a8c8f34093ba31df70d71b44111fdca
ArimoUg <92304384+ArimoUg@users.noreply.github.com>
2023-03-31 14:21:05 +0300

    Create taulukko.js

---
+++ taulukko.js
@@ -0,0 +1,15 @@
+const file = "guestbook.json";
+
+
+var results = '<table border="1" ';
+
+for (var i=0; i < file.length; i++) {
+   results +=
+   '<tr>' +
+   '<td>' + file[i].username + '</td>' +
+   '<td>' + file[i].country + '</td>' +
+   '<td>' + file[i].message + '</td>' +
+   '</tr>';
+}
+
+console.log(results);
